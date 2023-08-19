d4498fa0a73892c169c0e8a7e31ec7108390f1de
Sam-H420 <samuelherazo2@gmail.com>
2023-08-19 18:26:39 -0500

    a

--- src/patient_history.py
+++ src/patient_history.py
@@ -4,7 +4,7 @@ import a_classes as a
 
 class PatientHistory(a.Patient):
     """Class representing the clinical history of a patient"""
-    
+
     def __init__(self, patient, vital_signs, exam_results, medicaments, evolution_notes):
         super().__init__(patient.patient_id, patient.name, patient.genre, patient.birth_date)
         self.vital_signs = vital_signs
